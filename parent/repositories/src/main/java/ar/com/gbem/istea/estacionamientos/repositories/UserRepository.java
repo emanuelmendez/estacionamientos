1b42832af328081e43ec18370036bf05cc26b4e9
Emanuel Mendez <pielreloj@gmail.com>
2018-09-23 15:28:27 -0300

    Cambios para User
    Se elimina clase CrudRepository.
    Se corrije el UserRepository para utilizar el CrudRepository de Spring.
    Cambio de status code en creación de User
    Cambios de formato

--- parent/repositories/src/main/java/ar/com/gbem/istea/estacionamientos/repositories/UserRepository.java
+++ parent/repositories/src/main/java/ar/com/gbem/istea/estacionamientos/repositories/UserRepository.java
@@ -1,14 +1,15 @@
 package ar.com.gbem.istea.estacionamientos.repositories;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import ar.com.gbem.istea.estacionamientos.repositories.model.User;
 
-
 @Repository
-public interface UserRepository extends CrudRepository<User, Long>{
-	
+public interface UserRepository extends CrudRepository<User, Long> {
+
 	User findByPhone(String phone);
-	
+
 	User findByActive(int active);
+
 }
--- parent/web/src/main/java/ar/com/gbem/istea/estacionamientos/web/controllers/UserController.java
+++ parent/web/src/main/java/ar/com/gbem/istea/estacionamientos/web/controllers/UserController.java
@@ -18,29 +18,29 @@ import ar.gob.gbem.istea.estacionamientos.dtos.UserResultDTO;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<UserResultDTO>> listAllUsers() {
-        List<UserResultDTO> users = userService.findAllUsers();
-        if(users.isEmpty()){
-            return new ResponseEntity<List<UserResultDTO>>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<List<UserResultDTO>>(users, HttpStatus.OK);
-    }
-	
-	@RequestMapping(method = RequestMethod.POST,consumes = {"application/json"})
-	public @ResponseBody ResponseEntity<String> addNewUser(@RequestBody UserResultDTO userData) {
-		
-		userService.addUser(userData);
-		return new ResponseEntity<String>(HttpStatus.OK);
+	public ResponseEntity<List<UserResultDTO>> listAllUsers() {
+		List<UserResultDTO> users = userService.findAllUsers();
+		if (users.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST, consumes = { "application/json" })
+	public @ResponseBody ResponseEntity<String> addNewUser(@RequestBody(required = true) UserResultDTO userData) {
+
+		userService.addUser(userData);
+		return new ResponseEntity<>(HttpStatus.CREATED);
+	}
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserResultDTO> getUser(@PathVariable Long id) {
 		UserResultDTO user = userService.getUserById(id);
-		return new ResponseEntity<UserResultDTO>(user, HttpStatus.OK);
+		return new ResponseEntity<>(user, HttpStatus.OK);
 	}
 }
